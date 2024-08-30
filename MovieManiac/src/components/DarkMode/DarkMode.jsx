31b250e90e4945295ef8c2c8049b08d82781fffc
Abhishek Sinha <v-abhissinha@microsoft.com>
2024-08-31 01:15:04 +0530

    Implemented Dark and light theme

--- MovieManiac/src/components/DarkMode/DarkMode.jsx
+++ MovieManiac/src/components/DarkMode/DarkMode.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 
 import "./DarkMode.css";
-import sun from "./Sun.svg";
-import moon from "./Moon.svg";
 
 const DarkMode = () => {
 
+    const setDarkTheme = () => {
+        document.querySelector("body").setAttribute("data-theme", "dark")
+        localStorage.setItem("selectedTheme", "dark")
+    }
+    const setLightTheme = () => {
+        document.querySelector("body").setAttribute("data-theme", "light")
+        localStorage.setItem("selectedTheme", "light")
+    }
 
-    function MyMoon() {
-        return <img src={moon} alt="Moon Icon" />;
-      }
-    function MySun() {
-        return <img src={sun} alt="Moon Icon" />;
-      }
+    const selectedTheme = localStorage.getItem("selectedTheme")
+
+    if(selectedTheme=="light"){
+        setLightTheme();
+    }
+    else{
+        setDarkTheme();
+    }
+    const toggleTheme = (e) => {
+        if(e.target.checked){
+            setDarkTheme()
+        }
+        else{
+            setLightTheme()
+        }
+    }
+    
     return (
         <div className='dark_mode'>
             <input
                 className='dark_mode_input'
                 type='checkbox'
                 id='darkmode-toggle'
+                onChange={toggleTheme}
+                defaultChecked={selectedTheme !== 'light'}
             />
             <label className='dark_mode_label' htmlFor='darkmode-toggle'>
                
